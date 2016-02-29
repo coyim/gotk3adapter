e332556dbfc235dff93eff0866fbee7243b50932
Ola Bini <ola@olabini.se>
2016-02-29 19:50:09 +0100

    Add support for GTK mocks as well

---
+++ gtk_mock/application.go
@@ -0,0 +1,7 @@
+package gtk_mock
+
+import "github.com/twstrike/gotk3adapter/glib_mock"
+
+type MockApplication struct {
+	*glib_mock.MockApplication
+}
---
+++ gtk_mock/window.go
@@ -0,0 +1,35 @@
+package gtk_mock
+
+import (
+	"github.com/twstrike/gotk3adapter/gdki"
+	"github.com/twstrike/gotk3adapter/gtki"
+)
+
+type MockWindow struct {
+	*MockBin
+}
+
+func (*MockWindow) AddAccelGroup(v2 gtki.AccelGroup) {
+}
+
+func (*MockWindow) IsActive() bool {
+	return false
+}
+
+func (*MockWindow) Resize(v1, v2 int) {
+}
+
+func (*MockWindow) SetApplication(v2 gtki.Application) {
+}
+
+func (*MockWindow) SetIcon(v2 gdki.Pixbuf) {
+}
+
+func (*MockWindow) SetTitle(v1 string) {
+}
+
+func (*MockWindow) SetTitlebar(v2 gtki.Widget) {
+}
+
+func (*MockWindow) SetTransientFor(v2 gtki.Window) {
+}
