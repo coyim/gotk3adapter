9cd2dae7685ad043f50197936f0c852f2bdfc83a
Yakira Dixon <ydixon@thoughtworks.com>
2017-09-05 15:42:07 -0500

    Use gtki.Orientation in MockBox

--- gtk_mock/box.go
+++ gtk_mock/box.go
@@ -16,9 +16,9 @@ func (*MockBox) PackStart(v1 gtki.Widget, v2, v3 bool, v4 uint) {
 func (*MockBox) SetChildPacking(v1 gtki.Widget, v2, v3 bool, v4 uint, v5 gtki.PackType) {
 }
 
-func (*MockBox) GetOrientation() Orientation {
+func (*MockBox) GetOrientation() gtki.Orientation {
 	return gtki.HorizontalOrientation
 }
 
-func (*MockBox) SetOrientation(o Orientation) {
+func (*MockBox) SetOrientation(o gtki.Orientation) {
 }
